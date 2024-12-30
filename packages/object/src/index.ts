d1967644ad2768938ca39fa07ed6fa8473fd07b4
trungnotchung <trung09072004@gmail.com>
2024-12-30 12:05:22 +0700

    remove unused import

--- packages/object/src/index.ts
+++ packages/object/src/index.ts
@@ -13,7 +13,6 @@ import { ObjectSet } from "./utils/objectSet.js";
 
 export * as ObjectPb from "./proto/drp/object/v1/object_pb.js";
 export * from "./hashgraph/index.js";
-import { stat } from "node:fs";
 import { cloneDeep } from "es-toolkit";
 
 export interface IACL {
