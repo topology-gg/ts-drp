cf67eb56de5d184e902829e57db79f79f37d4998
Phạm Xuân Trung <66519569+trungnotchung@users.noreply.github.com>
2025-01-09 10:18:15 +0700

    Update packages/node/src/handlers.ts
    Co-authored-by: Oak <me+git@droak.sh>

--- packages/node/src/handlers.ts
+++ packages/node/src/handlers.ts
@@ -224,7 +224,7 @@ export async function signGeneratedVertices(node: DRPNode, vertices: Vertex[]) {
 		try {
 			await node.signVertex(vertex);
 		} catch (error) {
-			log.error("Error signing vertex:", vertex.hash, error);
+			log.error("::signGeneratedVertices: Error signing vertex:", vertex.hash, error);
 		}
 	});
 
