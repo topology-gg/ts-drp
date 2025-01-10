c62b1400940449d80a551fbbc03786506f880d47
trungnotchung <trung09072004@gmail.com>
2025-01-10 18:13:38 +0700

    add map blueprint

---
+++ packages/blueprints/src/Map/index.ts
@@ -0,0 +1,68 @@
+import {
+	ActionType,
+	type DRP,
+	type ResolveConflictsType,
+	SemanticsType,
+	type Vertex,
+} from "@ts-drp/object";
+
+export enum MapConflictResolution {
+	UpdateWins = 0,
+	RemoveWins = 1,
+}
+
+export class ConflictResolvingMap<K, V> implements DRP {
+	semanticsType = SemanticsType.pair;
+
+	private _conflictResolution: MapConflictResolution;
+	private _map: Map<K, V>;
+
+	constructor(conflictResolution?: MapConflictResolution) {
+		this._map = new Map();
+		this._conflictResolution =
+			conflictResolution ?? MapConflictResolution.UpdateWins;
+	}
+
+	update(key: K, value: V): void {
+		this._map.set(key, value);
+	}
+
+	remove(key: K): void {
+		this._map.delete(key);
+	}
+
+	query_has(key: K): boolean {
+		return this._map.has(key);
+	}
+
+	query_get(key: K): V | undefined {
+		return this._map.get(key);
+	}
+
+	resolveConflicts(vertices: Vertex[]): ResolveConflictsType {
+		if (!vertices[0].operation || !vertices[1].operation) {
+			return { action: ActionType.Nop };
+		}
+
+		const op1 = vertices[0].operation;
+		const op2 = vertices[1].operation;
+
+		if (op1.type === op2.type || op1.value !== op2.value) {
+			return { action: ActionType.Nop };
+		}
+
+		return this._conflictResolution === MapConflictResolution.UpdateWins
+			? {
+					action:
+						vertices[0].operation.type === "update"
+							? ActionType.DropRight
+							: ActionType.DropLeft,
+				}
+			: {
+					action:
+						vertices[0].operation.type === "update"
+							? ActionType.DropLeft
+							: ActionType.DropRight,
+				};
+	}
+}
