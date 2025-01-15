98a41f3eb4905279d1c5dac9b2b40b34983ae274
trungnotchung <trung09072004@gmail.com>
2025-01-15 17:25:22 +0700

    add more tests

--- packages/blueprints/tests/Map.test.ts
+++ packages/blueprints/tests/Map.test.ts
@@ -8,7 +8,18 @@ describe("ConflictResolvingMap tests", () => {
 		drp = new ConflictResolvingMap();
 	});
 
-	test("Test: Update value", () => {
+	test("Should add new entry", () => {
+		drp.update("key1", "value1");
+		drp.update("key2", "value2");
+		expect(drp.query_get("key1")).toBe("value1");
+		expect(drp.query_get("key2")).toBe("value2");
+		expect(drp.query_entries()).toEqual([
+			["key1", "value1"],
+			["key2", "value2"],
+		]);
+	});
+
+	test("Should update existing entries", () => {
 		drp.update("key1", "value1");
 		drp.update("key2", "value2");
 
@@ -17,9 +28,21 @@ describe("ConflictResolvingMap tests", () => {
 
 		drp.update("key1", "value3");
 		expect(drp.query_get("key1")).toBe("value3");
+
+		drp.update("key2", "value4");
+		expect(drp.query_get("key2")).toBe("value4");
 	});
 
-	test("Test: Update and remove value", () => {
+	test("Should update existing entries multiple times", () => {
+		drp.update("key1", "value1");
+		expect(drp.query_get("key1")).toBe("value1");
+		drp.update("key2", "value2");
+		drp.update("key2", "value3");
+		drp.update("key2", "value4");
+		expect(drp.query_get("key2")).toBe("value4");
+	});
+
+	test("Should update and remove existing entries", () => {
 		drp.update("key1", "value1");
 		expect(drp.query_get("key1")).toBe("value1");
 		drp.remove("key1");
@@ -32,5 +55,17 @@ describe("ConflictResolvingMap tests", () => {
 		drp.remove("key1");
 		expect(drp.query_has("key1")).toBe(false);
 		expect(drp.query_get("key2")).toBe("value3");
+	});
+
+	test("Should work correctly when remove is called on non-existing key", () => {
+		drp.update("key1", "value1");
+		drp.update("key2", "value2");
+		drp.remove("key3");
+		expect(drp.query_get("key1")).toBe("value1");
+		expect(drp.query_get("key2")).toBe("value2");
+		expect(drp.query_entries()).toEqual([
+			["key1", "value1"],
+			["key2", "value2"],
+		]);
 	});
 });
