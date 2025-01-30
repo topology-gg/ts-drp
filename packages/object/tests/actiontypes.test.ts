a1a997a54d8a55b0b3a13f4686910a6e5c30191b
droak <me+git@droak.sh>
2025-01-30 12:13:11 +0900

    basic test file structure

---
+++ packages/object/tests/actiontypes.test.ts
@@ -0,0 +1,25 @@
+import { AddMulDRP } from "@topology-foundation/blueprints";
+import { ActionType } from "@ts-drp/object";
+import { beforeEach, describe, expect, test } from "vitest";
+
+
+describe("Test ActionTypes", () => {
+	beforeEach(() => {
+	});
+
+	test("Test: Nop", () => {
+	});
+
+	test("Test: DropLeft", () => {
+	});
+
+	test("Test: DropRight", () => {
+	});
+
+	test("Test: Swap", () => {
+		const drp = new AddMulDRP();
+	});
+
+	test("Test: Drop", () => {
+	});
+});
