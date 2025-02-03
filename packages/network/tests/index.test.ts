0a929e8e7e5cbcef1be4c1cf87ea587a11d95b22
Sacha Froment <sfroment42@gmail.com>
2025-02-03 16:49:23 +0100

    chore:  add test
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>

--- packages/network/tests/index.test.ts
+++ packages/network/tests/index.test.ts
@@ -1,3 +1,35 @@
-import { test } from "vitest";
+import { beforeAll, describe, expect, test } from "vitest";
 
-test("mock", () => {});
+import { DRPNetworkNode } from "../src/node.js";
+
+describe("isDialable", () => {
+	let btNode: DRPNetworkNode;
+	beforeAll(async () => {
+		btNode = new DRPNetworkNode({
+			bootstrap: true,
+			listen_addresses: ["/ip4/0.0.0.0/tcp/0/ws"],
+			bootstrap_peers: [],
+			private_key_seed: "bootstrap_is_dialable",
+		});
+		await btNode.start();
+	});
+
+	test("should return true if the node is dialable", async () => {
+		const node = new DRPNetworkNode({
+			bootstrap_peers: btNode.getMultiaddrs()?.map((addr) => addr.toString()) || [],
+			private_key_seed: "is_dialable_node_1",
+		});
+		await node.start();
+		expect(await node.isDialable(100)).toBe(true);
+	});
+
+	test("should return false if the node is not dialable", async () => {
+		const node = new DRPNetworkNode({
+			bootstrap_peers: btNode.getMultiaddrs()?.map((addr) => addr.toString()) || [],
+			private_key_seed: "is_dialable_node_2",
+			listen_addresses: [],
+		});
+		await node.start();
+		expect(await node.isDialable(100)).toBe(false);
+	});
+});
