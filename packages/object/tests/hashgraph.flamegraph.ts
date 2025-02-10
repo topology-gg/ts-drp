c7af452ac0631e6fccb1a4607d973b377d3e8b7c
anhnd350309 <anhnd350309@gmail.com>
2025-02-10 18:51:39 +0700

    feat: add pprof

--- packages/object/tests/hashgraph.flamegraph.ts
+++ packages/object/tests/hashgraph.flamegraph.ts
@@ -1,4 +1,6 @@
 import { SetDRP } from "@ts-drp/blueprints/src/index.js";
+import fs from "fs";
+import * as pprof from "pprof";
 
 import { DRPObject, ObjectACL } from "../src/index.js";
 
@@ -62,5 +64,22 @@ function flamegraphForSetDRP(numDRPs: number, verticesPerDRP: number, mergeFn: b
 		mergeObjects(objects);
 	}
 }
+
+async function prof() {
+	console.log("start to profile >>>");
+	const profile = await pprof.time.profile({
+		durationMillis: 5000,
+	});
+
+	const buf = await pprof.encode(profile);
+	fs.writeFile("flamegraph.pb.gz", buf, (err) => {
+		if (err) {
+			throw err;
+		}
+	});
+	console.log("<<< finished to profile");
+}
+
+prof().catch(console.error);
 
 flamegraphForSetDRP(1, 1000, false);
