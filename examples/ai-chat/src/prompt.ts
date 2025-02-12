11a459e26942a221e40b496ea396e6103139a228
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2025-02-12 18:53:26 +0100

    prompt

--- examples/ai-chat/src/prompt.ts
+++ examples/ai-chat/src/prompt.ts
@@ -13,12 +13,19 @@
 // 	- No message ends in a question.
 // `
 
-export const systemPrompt = `
-	if the latest message is a single number, double it and send the result as message,
-	do not say anything else - your message should just be a single number.
-`
+// export const systemPrompt = `
+// 	if the latest message is a single number, double it and send the result as message,
+// 	do not say anything else - your message should just be a single number.
+// `
 
 // export const systemPrompt = `
 // 	assess the atmosphere of the chat history, and pick an emoji that is most opposing the atmosphere.
 // 	do not say anything else. your message should just be a single emoji.
 // `
+
+export const systemPrompt = `
+	If the last message is a question, answer it with an opinion within 32 words and take a position in doing so.
+	Otherwise, generate a message that both positionally opposes the last message, consistent with the past position you already took, and also brings new information and argument that do not already exist in the chat history.
+	Do not state your intent, such as starting your response with "The last message isn't a question, so I'll oppose it with new information."
+	Your response is just the opinion itself that is less than 32 words.
+`
