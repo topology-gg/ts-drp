dedc779f28b6d72ee75661993c7fb7dc7e551381
Sacha Froment <sfroment42@gmail.com>
2025-02-24 11:35:49 +0100

    fix(examples/grid): :bug: bug where eventListener is added multiple time
    chore: better setinterval
    
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>
    
    chore: try to fix cache
    
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>
    
    chore: test bench
    
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>
    
    chore: test bench
    
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>
    
    chore: remove test file
    
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>
    
    change when cache is set
    
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>
    
    change when cache is set
    
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>
    
    fix: cache
    
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>
    
    fix: cache
    
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>
    
    123
    
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>

--- examples/grid/src/index.ts
+++ examples/grid/src/index.ts
@@ -92,8 +92,14 @@ async function run(metrics?: IMetrics) {
 	});
 
 	const button_connect = <HTMLButtonElement>document.getElementById("joinGrid");
+	const grid_input = <HTMLInputElement>document.getElementById("gridInput");
+	grid_input.addEventListener("keydown", (event) => {
+		if (event.key === "Enter") {
+			button_connect.click();
+		}
+	});
 	button_connect.addEventListener("click", async () => {
-		const drpId = (<HTMLInputElement>document.getElementById("gridInput")).value;
+		const drpId = grid_input.value;
 		try {
 			gridState.drpObject = await gridState.node.connectObject({
 				id: drpId,
@@ -138,15 +144,19 @@ async function main() {
 		metrics = new OpentelemetryMetrics("grid-service-2");
 	}
 
+	let hasRun = false;
+
 	const networkConfig = getNetworkConfigFromEnv();
 	gridState.node = new DRPNode(networkConfig ? { network_config: networkConfig } : undefined);
 	await gridState.node.start();
 	await gridState.node.networkNode.isDialable(async () => {
 		console.log("Started node", import.meta.env);
+		if (hasRun) return;
+		hasRun = true;
 		await run(metrics);
 	});
 
-	setInterval(renderInfo, import.meta.env.VITE_RENDER_INFO_INTERVAL);
+	if (!hasRun) setInterval(renderInfo, import.meta.env.VITE_RENDER_INFO_INTERVAL);
 }
 
 void main();
